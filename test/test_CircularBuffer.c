e709abf7b6f6fbf880dc876ca50fe5d251e47123
WONG KEAT HONG <Raydivine@users.noreply.github.com>
2014-06-16 17:19:59 +0800

    Update test_CircularBuffer.c

--- test/test_CircularBuffer.c
+++ test/test_CircularBuffer.c
@@ -11,83 +11,88 @@ void tearDown(void)
 }
 
 
-void test_circularBufferNew_given_6_should_allocate_CircularBuffer_object_with_a_buffer_of_6(void) {
 
+
+void test_circularBufferNew_given_6_should_allocate_CircularBuffer_object_with_a_buffer_of_6(void){
 	CircularBuffer *cb = circularBufferNew(6);
+	
 	TEST_ASSERT_NOT_NULL(cb);
 	TEST_ASSERT_NOT_NULL(cb->buffer);
 	TEST_ASSERT_EQUAL(6, cb->length);
 	TEST_ASSERT_EQUAL(0, cb->size);
 	TEST_ASSERT_EQUAL_PTR(cb->buffer, cb->head);
-	TEST_ASSERT_EQUAL_PTR(cb->buffer, cb->tail);	
-	circularBufferDel(cb);
+	TEST_ASSERT_EQUAL_PTR(cb->buffer, cb->tail);
 	
+	circularBufferDel(cb);
 }
 
-void test_circulrBufferAdd_given_length6and_add_OneNumber_should_add_2_to_the_tail(void){
-    
-	circularBufferNew(6);
-	struct Buffer CircularBuffer;
+
+void test_circularBufferAdd_given_length3_and_add_3value_should_not_give_error(void){
+	CircularBuffer *cb = circularBufferNew(3);
 	
     CEXCEPTION_T	err;
-	Try {
-		circularBufferAdd( &CircularBuffer, 2 ) ;
+	Try {			
+		    circularBufferAdd(cb, 9);
+			circularBufferAdd(cb, 9);
+			circularBufferAdd(cb, 9);
+		
 		printf("Success: No exception generated.\n");
 	} Catch(err) {
 		TEST_FAIL_MESSAGE("Do not expect exception to be generated.")	
 	}	
 }
-	
-void test_circulrBufferAdd_given_length5_and_add_6Number_should_given_full_buffer_error(void){
-    
-	circularBufferNew(5);
-	struct Buffer CircularBuffer;
-	
+
+
+void test_circularBufferAdd_given_length4_and_add_5_number_should_give_full_buffer_error(void){
+	CircularBuffer *cb = circularBufferNew(4);
+
+	CEXCEPTION_T err;
+
+	Try{
+			circularBufferAdd( cb, 9 );
+			circularBufferAdd( cb, 9 );
+			circularBufferAdd( cb, 9 );
+			circularBufferAdd( cb, 9 );
+			circularBufferAdd( cb, 9 );		
+	}	Catch(err) 	{   
+				TEST_ASSERT_EQUAL(ERR_BUFFER_IS_FULL, err);
+				printf("Success: Exception generated. Error code: %d.\n", err);
+		}
+}
+
+void test_circularBufferRemove_given_length2_remove_2value_should_not_give_error(void){
+	CircularBuffer *cb = circularBufferNew(2);
+   
     CEXCEPTION_T	err;
-	Try {
-		circularBufferAdd( &CircularBuffer, 1 ) ;
-		circularBufferAdd( &CircularBuffer, 2 ) ;
-		circularBufferAdd( &CircularBuffer, 3 ) ;
-		circularBufferAdd( &CircularBuffer, 4 ) ;
-		circularBufferAdd( &CircularBuffer, 5 ) ;
-		circularBufferAdd( &CircularBuffer, 6 ) ;
-		TEST_FAIL_MESSAGE("Should generate an error due to full buffer.")
+	
+	int PopData;
+
+	Try {				    
+			circularBufferAdd(cb, 9);
+			circularBufferAdd(cb, 9);
+			PopData = circularBufferRemove(cb);
+			printf("Success: No exception generated.\n");
+		
 	} Catch(err) {
-		TEST_ASSERT_EQUAL(ERR_BUFFER_IS_FULL, err);
-	}	
+			TEST_FAIL_MESSAGE("Do not expect exception to be generated.")	
+				}				
 }
 
 
-void test_circulrBufferAdd_given_length0_and_add_2Number_should_given_null_buffer_error(void){
-   
-     circularBufferNew(0);
-	 struct Buffer CircularBuffer;
 
-	 CEXCEPTION_T	err;
-	Try {
-		circularBufferAdd( &CircularBuffer, 4 ) ;
-		circularBufferAdd( &CircularBuffer, 5 ) ;
-		TEST_FAIL_MESSAGE("Should generate an error due to null buffer.")
-	} Catch(err) {
-		TEST_ASSERT_EQUAL(ERR_BUFFER_IS_EMPTY, err);	
-	}	
+
+void test_circularBufferRemove_given_length0_should_give_buffer_empty_error(void){
+	CircularBuffer *cb = circularBufferNew(0);
+	
+	CEXCEPTION_T err;
+	
+	Try{
+		circularBufferRemove(cb);
+	}Catch(err){
+		TEST_ASSERT_EQUAL(ERR_BUFFER_IS_EMPTY, err);
+		printf("Success: Exception generated. Error code: %d.\n", err);
+	}
 }
-
-
-void test_circularBufferRemove_given_length1_and_remove_1Number_should_set_to_null(void){
-   
-     circularBufferNew(1);
-	 struct Buffer CircularBuffer;
-
-	 CEXCEPTION_T	err;
-	Try {
-		circularBufferRemove(&CircularBuffer) ;
-		TEST_FAIL_MESSAGE("Should generate an error due to null buffer.")
-	} Catch(err) {
-		TEST_ASSERT_EQUAL(ERR_BUFFER_IS_EMPTY, err);	
-	}	
-}
-
 
 
 
